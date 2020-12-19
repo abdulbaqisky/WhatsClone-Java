d8f5254e1f4353599a5b40f9842c48b0437149d5
Adekomi AbdulBaaqi <adekomiabdulbaqi3@gmail.com>
2020-12-19 09:22:01 +0100

    corrected toolbar layout

--- app/src/main/java/com/example/whatsclone/viewPagerAdapter.java
+++ app/src/main/java/com/example/whatsclone/viewPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.whatsclone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -14,11 +15,27 @@ public class viewPagerAdapter extends FragmentPagerAdapter {
   @NonNull
   @Override
   public Fragment getItem(int position) {
+    switch (position){
+      case 0: return new Chats();
+      case 1: return new Status();
+      case 2: return new Calls();
+    }
     return null;
   }
 
   @Override
   public int getCount() {
     return 3;
+  }
+
+  @Nullable
+  @Override
+  public CharSequence getPageTitle(int position) {
+    switch (position){
+      case 0: return "CHATS";
+      case 1: return "STATUS";
+      case 2: return "CALLS";
+    }
+    return null;
   }
 }
